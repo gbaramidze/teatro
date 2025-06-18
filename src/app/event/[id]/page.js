51cc679890fd9161c91667a7eb50952db73b266c
gbaramidze <baramidzegio@gmail.com>
2025-06-18 17:36:42 +0400

    changes

--- src/app/event/[id]/page.js
+++ src/app/event/[id]/page.js
@@ -15,12 +15,9 @@ const Share = dynamic(() => import('./Share'), {
 
 export async function generateMetadata({ params }) {
   const { id } = params;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) return {};
-
   await connectToDatabase();
   const event = await Event.findById(id).lean();
-  if (!event) return {};
+  if (!event) return notFound();
 
   return {
     title: event.title,
@@ -41,8 +38,54 @@ export default async function EventPage({ params }) {
 
   if (!event) return notFound();
 
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Event',
+    name: event.title,
+    image: event.image,
+    description: event.description,
+    eventStatus: "https://schema.org/EventScheduled",
+    startDate: `${dayjs(event.date).format("YYYY-MM-DDT00:00:00")}`,
+    endDate: `${dayjs(event.date).add(1, 'day').format("YYYY-MM-DDT06:00:00")}`,
+    location: {
+      "@type": "place",
+      "name": "Teatro",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Lech and Maria kachenski street 5b",
+        "addressLocality": "Batumi",
+        "addressRegion": "Adjara",
+        "postalCode": "6000",
+        "addressCountry": "GE"
+      },
+    },
+    offers: {
+      "@type": "Offer",
+      "url": "https://teatro.ge/events/" + id,
+      "price": event.activePrice,
+      "priceCurrency": "GEL",
+      "availability": "https://schema.org/InStock",
+      "validFrom": "2025-06-12T12:00"
+    },
+    "performer": {
+      "@type": "PerformingGroup",
+      "name": event.title,
+    },
+    "organizer": {
+      "@type": "Organization",
+      "name": "Teatro",
+      "url": "https://teatro.ge"
+    }
+  }
+
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, '\\u003c'),
+        }}
+      />
       <PageHeader currentPage={event.title} isBlogDetails={"Events"} banner={"banner-1 banner-2"} />
       <Container className={"mt-5"}>
         <Row>
